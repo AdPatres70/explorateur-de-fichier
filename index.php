214bf1a42456d319581f613b8bcc488f75ad1884
AdPatres70 <alexandre.r@codeur.online>
2018-11-12 17:02:39 +0100

    bravo

--- index.php
+++ index.php
@@ -9,19 +9,34 @@ $twig = new Twig_Environment($loader, array(
 
 $nb_fichier = 0;
     $listefichier = array();
-
+    //Ouverture du dossier "test"
     if($dossier = opendir('test'))
     {
+        //On boucle tant que l'on trouve un dossier ou un fichier dans le dossier/chemin - test
         while(false !== ($fichier = readdir($dossier)))
         {
+            //On ignore les fichiers . .. et index.php
             if($fichier != '.' && $fichier != '..' && $fichier != 'index.php')
             {
+                //On incrémente la variable nb_fichier de +1
                 $nb_fichier++;
+                //On ajout le dossier ou le fichier trouvé dans un tableau multidimentionnel
                 $listefichier[] = $fichier;
             }
         }  
+        //On ferme le dosser ouvert - test
         closedir($dossier);
     }
-$template = $twig->load('explorateur.twig');
-echo $template->render(array("listefichier"=>$listefichier)); 
+
+//On indique au moteur de templating le fichier html à ouvrir
+$template = $twig->load('index.twig');
+
+
+//On assigne les varibles devant être affiché au fichier
+echo $template->render(
+    array(
+        'prenom' => 'boussad',
+        "listefichier"=>$listefichier
+    )
+); 
  ?>
