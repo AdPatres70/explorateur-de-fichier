bdab8892f342ef2272e9798dae628641bc65a841
AdPatres70 <alexandre.r@codeur.online>
2018-11-13 14:55:25 +0100

    PHP

--- index.php
+++ index.php
@@ -6,23 +6,24 @@ $twig = new Twig_Environment($loader, array(
     'cache' => false,
 ));
 
+$chemin = $_GET ['chemin'];
 
 $nb_fichier = 0;
     $listefichier = array();
     //Ouverture du dossier "test"
-    if($dossier = opendir('test'))
+    if($dossier = opendir($chemin))
     {
         //On boucle tant que l'on trouve un dossier ou un fichier dans le dossier/chemin - test
         while(false !== ($fichier = readdir($dossier)))
         {
             //On ignore les fichiers . .. et index.php
-            if($fichier != '.' && $fichier != '..' && $fichier != 'index.php')
-            {
+            // if($fichier != '.' && $fichier != '..' && $fichier != 'index.php')
+            // {
                 //On incrémente la variable nb_fichier de +1
                 $nb_fichier++;
                 //On ajout le dossier ou le fichier trouvé dans un tableau multidimentionnel
                 $listefichier[] = $fichier;
-            }
+            // }
         }  
         //On ferme le dosser ouvert - test
         closedir($dossier);
@@ -36,7 +37,8 @@ $template = $twig->load('index.twig');
 echo $template->render(
     array(
         
-        "listefichier"=>$listefichier
+        "listefichier"=>$listefichier,
+        "chemin"=>$chemin
     )
 ); 
  ?>
